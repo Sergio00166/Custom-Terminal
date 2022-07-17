8e1c67355a14302753a1754bf87b4de26fb18c5c
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2022-07-17 18:50:05 +0200

    Update Terminal.py

--- Terminal.py
+++ Terminal.py
@@ -7,7 +7,7 @@ from threading import Thread as thr
 from time import sleep as delay
 from os import getcwd
 
-version="pre-alfa v0.0.7.2"
+version="pre-alfa v0.0.7.4"
 
 dirt=getcwd()
 path[5]=(dirt + chr(92) + "import")

d36c56d26161c1f32de27836afc2b902d75f8d9c
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2022-07-22 15:03:38 +0200

    New cli calculator

---
+++ import/calculator/cli.py
@@ -0,0 +1,28 @@
+from os import system as cmd
+import math
+
+def work(arg):
+    for x in dir(math):
+        arg=str(arg).replace(x,"math."+x)
+    arg=str(arg).replace("^","**")
+    fic=open("tmp.py","w")
+    fic.write("import math \n")
+    fic.write("print('Result:' , ")
+    fic.write(arg)
+    fic.write(")")
+    fic.close()
+    cmd("start /B /WAIT tmp.py")
+    
+def main():
+    arg=""
+    while True:
+        print("")
+        arg=input("Operation: ")
+        if not arg=="exit":
+            work(arg)
+        else:
+            exit()
+
+if __name__=="__main__":
+    print(" CLI CALCULATOR by Sergio1260")
+    main()

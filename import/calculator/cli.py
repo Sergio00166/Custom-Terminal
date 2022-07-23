63f0a92afceff13fc8d1017d88accccdbc6680a2
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2022-07-23 20:55:46 +0200

    Update cli.py

--- import/calculator/cli.py
+++ import/calculator/cli.py
@@ -2,9 +2,15 @@ from os import system as cmd
 import math
 from sys import modules
 
+letters={":a":0,":b":0,":c":0,":d":0}
+
 def work(arg,mode):
+    global letters
     for x in dir(math):
         arg=str(arg).replace(x,"math."+x)
+    for x in letters:
+        arg=arg.replace(x,str(letters[x]))
+    arg=arg.replace("#","")
     fic=open("tmp.py","w")
     fic.write("import math \n")
     fic.write("def operator(): \n")
@@ -28,9 +34,9 @@ def work(arg,mode):
             print("   ERROR")
 
 def main():
+    global letters
     fix=False
     arg=""
-    letters={"a":0,"b":0,"c":0,"d":0}
     while True:
         if fix==True:
             work(arg,3)
@@ -41,12 +47,10 @@ def main():
                 if "set" in arg:
                     letter=arg[arg.find("set(")+4:arg.find(")")]
                     for x in letters:
-                        if x==letter:
-                            letters[x]=work(arg[arg.find(")")+1:],2)
+                        if x.replace(":","")==letter:
+                            letters[x]=work(arg.replace("set(" + letter + ")",""),2)
                             fix=True        
                 else:
-                    for x in letters:
-                        arg=arg.replace(x,str(letters[x]))
                     work(arg,1)
             else:
                 print("")
@@ -56,6 +60,6 @@ def main():
 if __name__=="__main__":
     print("")
     print(" CLI CALCULATOR by Sergio1260")
-    print(" version v0.2.5 - STABLE")
+    print(" version v0.3.0 - STABLE")
     main()
 
